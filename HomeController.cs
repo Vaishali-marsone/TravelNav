b60734f3336c778279d5a596af324380c7641f31
Vaishali Parmeshwar Marsone <82966052+Vaishali-marsone@users.noreply.github.com>
2023-08-04 01:14:42 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace demo1
+{
+    public class HomeController : Controller
+    {
+        private SampleDBContext db=new SampleDBContext()
+        public IActionResult Index(string searchBy,string search)
+
+        {
+            if(searchBy=="Source")
+            {
+                return View(db.Vehicles1.Where(X => x.Source == search || search == null).ToList());
+            }
+
+            if (searchBy == "Destination")
+
+            {
+                return View(db.Vehicles1.Where(X => x.Destination == StartWith(search) || search == null).ToList());
+
+            }
+
+
+            return View();
+        }
+    }
+
+    internal class SampleDBContext
+    {
+        public SampleDBContext()
+        {
+        }
+    }
+}
